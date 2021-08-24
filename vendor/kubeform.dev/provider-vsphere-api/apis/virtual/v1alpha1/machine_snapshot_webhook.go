65078aec16d94c602befcfe4d403df50557ecfc4
1gtm <1gtm@appscode.com>
2021-08-24 08:27:01 +0000

    Generate code for provider@v1.26.1-0.20210625182906-b8206ea7fc5a gen@ab9b830
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- vendor/kubeform.dev/provider-vsphere-api/apis/virtual/v1alpha1/machine_snapshot_webhook.go
+++ vendor/kubeform.dev/provider-vsphere-api/apis/virtual/v1alpha1/machine_snapshot_webhook.go
@@ -20,9 +20,12 @@ package v1alpha1
 
 import (
 	"fmt"
+	"strings"
 
 	base "kubeform.dev/apimachinery/api/v1alpha1"
+	"kubeform.dev/provider-vsphere-api/util"
 
+	jsoniter "github.com/json-iterator/go"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
@@ -38,6 +41,16 @@ func (r *MachineSnapshot) SetupWebhookWithManager(mgr ctrl.Manager) error {
 
 var _ webhook.Validator = &MachineSnapshot{}
 
+var machinesnapshotForceNewList = map[string]bool{
+	"/consolidate":          true,
+	"/description":          true,
+	"/memory":               true,
+	"/quiesce":              true,
+	"/remove_children":      true,
+	"/snapshot_name":        true,
+	"/virtual_machine_uuid": true,
+}
+
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type
 func (r *MachineSnapshot) ValidateCreate() error {
 	return nil
@@ -45,6 +58,53 @@ func (r *MachineSnapshot) ValidateCreate() error {
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
 func (r *MachineSnapshot) ValidateUpdate(old runtime.Object) error {
+	if r.Spec.Resource.ID == "" {
+		return nil
+	}
+	newObj := r.Spec.Resource
+	res := old.(*MachineSnapshot)
+	oldObj := res.Spec.Resource
+
+	jsnitr := jsoniter.Config{
+		EscapeHTML:             true,
+		SortMapKeys:            true,
+		TagKey:                 "tf",
+		ValidateJsonRawMessage: true,
+		TypeEncoders:           GetEncoder(),
+		TypeDecoders:           GetDecoder(),
+	}.Froze()
+
+	byteNew, err := jsnitr.Marshal(newObj)
+	if err != nil {
+		return err
+	}
+	tempNew := make(map[string]interface{})
+	err = jsnitr.Unmarshal(byteNew, &tempNew)
+	if err != nil {
+		return err
+	}
+
+	byteOld, err := jsnitr.Marshal(oldObj)
+	if err != nil {
+		return err
+	}
+	tempOld := make(map[string]interface{})
+	err = jsnitr.Unmarshal(byteOld, &tempOld)
+	if err != nil {
+		return err
+	}
+
+	for key := range machinesnapshotForceNewList {
+		keySplit := strings.Split(key, "/*")
+		length := len(keySplit)
+		checkIfAnyDif := false
+		util.CheckIfAnyDifference("", keySplit, 0, length, &checkIfAnyDif, tempOld, tempOld, tempNew)
+		util.CheckIfAnyDifference("", keySplit, 0, length, &checkIfAnyDif, tempNew, tempOld, tempNew)
+
+		if checkIfAnyDif && r.Spec.UpdatePolicy == base.UpdatePolicyDoNotDestroy {
+			return fmt.Errorf(`machinesnapshot "%v/%v" immutable field can't be updated. To update, change spec.updatePolicy to Destroy`, r.Namespace, r.Name)
+		}
+	}
 	return nil
 }
 
--- vendor/kubeform.dev/provider-vsphere-api/apis/virtual/v1alpha1/machine_webhook.go
+++ vendor/kubeform.dev/provider-vsphere-api/apis/virtual/v1alpha1/machine_webhook.go
@@ -20,9 +20,12 @@ package v1alpha1
 
 import (
 	"fmt"
+	"strings"
 
 	base "kubeform.dev/apimachinery/api/v1alpha1"
+	"kubeform.dev/provider-vsphere-api/util"
 
+	jsoniter "github.com/json-iterator/go"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
@@ -38,6 +41,19 @@ func (r *Machine) SetupWebhookWithManager(mgr ctrl.Manager) error {
 
 var _ webhook.Validator = &Machine{}
 
+var machineForceNewList = map[string]bool{
+	"/network_interface/*/ovf_mapping":       true,
+	"/ovf_deploy/*/deployment_option":        true,
+	"/ovf_deploy/*/disk_provisioning":        true,
+	"/ovf_deploy/*/enable_hidden_properties": true,
+	"/ovf_deploy/*/ip_allocation_policy":     true,
+	"/ovf_deploy/*/ip_protocol":              true,
+	"/ovf_deploy/*/local_ovf_path":           true,
+	"/ovf_deploy/*/ovf_network_map":          true,
+	"/ovf_deploy/*/remote_ovf_url":           true,
+	"/replace_trigger":                       true,
+}
+
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type
 func (r *Machine) ValidateCreate() error {
 	return nil
@@ -45,6 +61,53 @@ func (r *Machine) ValidateCreate() error {
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
 func (r *Machine) ValidateUpdate(old runtime.Object) error {
+	if r.Spec.Resource.ID == "" {
+		return nil
+	}
+	newObj := r.Spec.Resource
+	res := old.(*Machine)
+	oldObj := res.Spec.Resource
+
+	jsnitr := jsoniter.Config{
+		EscapeHTML:             true,
+		SortMapKeys:            true,
+		TagKey:                 "tf",
+		ValidateJsonRawMessage: true,
+		TypeEncoders:           GetEncoder(),
+		TypeDecoders:           GetDecoder(),
+	}.Froze()
+
+	byteNew, err := jsnitr.Marshal(newObj)
+	if err != nil {
+		return err
+	}
+	tempNew := make(map[string]interface{})
+	err = jsnitr.Unmarshal(byteNew, &tempNew)
+	if err != nil {
+		return err
+	}
+
+	byteOld, err := jsnitr.Marshal(oldObj)
+	if err != nil {
+		return err
+	}
+	tempOld := make(map[string]interface{})
+	err = jsnitr.Unmarshal(byteOld, &tempOld)
+	if err != nil {
+		return err
+	}
+
+	for key := range machineForceNewList {
+		keySplit := strings.Split(key, "/*")
+		length := len(keySplit)
+		checkIfAnyDif := false
+		util.CheckIfAnyDifference("", keySplit, 0, length, &checkIfAnyDif, tempOld, tempOld, tempNew)
+		util.CheckIfAnyDifference("", keySplit, 0, length, &checkIfAnyDif, tempNew, tempOld, tempNew)
+
+		if checkIfAnyDif && r.Spec.UpdatePolicy == base.UpdatePolicyDoNotDestroy {
+			return fmt.Errorf(`machine "%v/%v" immutable field can't be updated. To update, change spec.updatePolicy to Destroy`, r.Namespace, r.Name)
+		}
+	}
 	return nil
 }
 
